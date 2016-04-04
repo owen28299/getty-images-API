e5396bf9ba185e73d3949e97ff53e90403a4ab7b
Owen <owenyangg@gmail.com>
2016-04-04 12:07:12 -1000

    loads images

--- public/js/app.js
+++ public/js/app.js
@@ -4,17 +4,25 @@ document.getElementById('search').addEventListener('click', function(){
   var imgRequest = new XMLHttpRequest();
   imgRequest.addEventListener('load', render);
   imgRequest.open('GET',"https://api.gettyimages.com/v3/search/images?phrase=" + query);
-
+  imgRequest.setRequestHeader('Api-key', key);
   imgRequest.send();
 
 });
 
 function render(){
+  document.getElementById('feed').innerHTML = "";
 
   var images = JSON.parse(this.responseText).images;
+  var temp = document.createDocumentFragment();
 
   images.forEach(function(element){
     var imgURL = element.display_sizes[0].uri;
+    var img = document.createElement('img');
+    img.src = imgURL;
+    temp.appendChild(img);
+
   });
+
+  document.getElementById('feed').appendChild(temp);
 
 }
