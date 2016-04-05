1bf1037b5ffac75ff031e5f9a7208495060be916
Owen <owenyangg@gmail.com>
2016-04-04 16:27:07 -1000

    adds page reload on scroll to bottom and history

--- public/README.md
+++ public/README.md
@@ -1,3 +1,33 @@
-# getty-images-API
-# getty-images-API
-# getty-images-API
+Getty Images Search Engine
+Create an account
+
+Go to https://developer.gettyimages.com/login/
+Create an account.
+
+Be sure to select this option when signing up:
+image
+
+Read the Documentation
+
+Link to the Docs
+
+Goal
+
+You are building an interface which will search Getty Images for a particular set of photos by keyword (or more) and then display the images to the user. Your interface must at the very minimum meet these requirements:
+
+A way to take user input.
+A button to start the search
+Once you receive the response from the server with the image urls, display the images on the web app.
+Caveats
+
+As you read in the Docs (you did read the docs, right?), you will need to craft a custom header for your API KEY with Vanilla Javascript XHR request.
+Stretch Goals
+
+Add the ability to store the history of search terms. Consider a side-menu with the search terms and when those words are clicked it will trigger a new request to gettyimages and display the results to the interface.
+Uber World Boss Spawn When you reload the page, the history is gone! How do you save your history? warning: we did not cover this in class yet, in the mood for an adventure?
+Can you think of a way to use this in conjuction with the OpenWeather API?
+Ahem if the forecast for the day in "Cloudy" then fire off a request to gettyimages for "cloudy" images (or videos).
+Resources
+
+http://slides.com/sgnl/xhr#/
+GettyImages documentation
--- public/js/app.js
+++ public/js/app.js
@@ -1,17 +1,44 @@
-document.getElementById('search').addEventListener('click', function(){
-  var query = document.getElementById('query').value;
+function Page(){
+  this.pageNum = 1;
+  this.history = [];
+}
 
+window.onload = function(){
+  var weatherRequest = new XMLHttpRequest();
+  var weather = "sunny";
+  var city = "honolulu, us";
+  var page = new Page();
+  page.history = localStorage.getItem('history').split(",");
+  appendHistory();
+
+  weatherRequest.addEventListener('load', function(){
+    weather = JSON.parse(this.responseText).list[0].weather[0].main;
+    document.getElementById('query').value = weather;
+    imgXML(weather);
+  });
+  weatherRequest.open('GET',"http://api.openweathermap.org/data/2.5/forecast?q=" + city + "&mode=JSON&appid=" + weatherKey);
+  weatherRequest.send();
+
+function imgXML(query){
   var imgRequest = new XMLHttpRequest();
   imgRequest.addEventListener('load', render);
-  imgRequest.open('GET',"https://api.gettyimages.com/v3/search/images?phrase=" + query);
+  imgRequest.open('GET',"https://api.gettyimages.com/v3/search/images?page=" + page.pageNum + "&phrase=" + query);
   imgRequest.setRequestHeader('Api-key', key);
   imgRequest.send();
+}
+
+document.getElementById('search').addEventListener('click', function(event){
+  event.preventDefault();
+  var query = document.getElementById('query').value;
+  document.getElementById('feed').innerHTML = "";
+  imgXML(query);
+
+  page.history.push(query);
+  localStorage.setItem('history', page.history);
 
 });
 
 function render(){
-  document.getElementById('feed').innerHTML = "";
-
   var images = JSON.parse(this.responseText).images;
   var temp = document.createDocumentFragment();
 
@@ -24,5 +51,35 @@ function render(){
   });
 
   document.getElementById('feed').appendChild(temp);
+}
+
+function testScroll(event){
+
+  event.preventDefault();
+
+  console.log(localStorage.getItem('history'));
+
+  if(window.innerHeight + window.scrollY > document.body.offsetHeight){
+    page.pageNum += 1;
+    var query = document.getElementById('query').value;
+    imgXML(query);
+  }
+}
+
+window.onscroll = testScroll;
+
+function appendHistory(){
+  var selectH = document.getElementById('dropdown');
+
+  page.history.forEach(function(element){
+
+    var option = document.createElement('option');
+    option.value = element;
+    option.innerHTML = element;
+
+    selectH.appendChild(option);
+  });
 
 }
+
+};
